ee3939a12bb1b5fee064bb9e6ad1b912e2dd1da6
Ahmed Sayed <ahmedsayed22988@yahoo.com>
2018-10-26 17:02:19 +0200

    BigInt-Ahmed

--- main.cpp
+++ main.cpp
@@ -1,74 +1,187 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-class bigint{
-    string num;
+
+class BigInt
+{
 public:
-    bigint(){}
-    void del(bigint b){}
-    bigint(string x){
-        num=x;
-    }
-    void del(){
-        for(int i=0;i<num.size();++i){
-            if(num[i]=='0') num.erase(num.begin()),i--;
-            else break;
-        }
-    }
-    string print(){
-        return num;
-    }
-   bigint operator-(bigint b){
-       this->del();
-       b.del();
-       string bigNum,smalNum,subtract;
-       bigint tmp;
-       bool sign;
-       if(b.num.size()>this->num.size()){
-            bigNum=b.num;
-            smalNum=this->num;
-            sign=0;
-       }
-       else if(b.num.size()<this->num.size()){
-            bigNum=this->num;
-            smalNum=b.num;
-            sign=1;
-       }
-       else{
-        for(int i=0;i<b.num.size();++i)
-            if(b.num[i]>this->num[i]) {bigNum=b.num,smalNum=this->num,sign=0;break;}
-            else if(b.num[i]<this->num[i]) {bigNum=this->num,smalNum=b.num,sign=1;break;}
-       }
-       int i;
-       reverse(smalNum.begin(),smalNum.end());
-       reverse(bigNum.begin(),bigNum.end());
-       for(i=0;i<smalNum.size();++i){
-        if(bigNum[i]-'0'<smalNum[i]-'0') bigNum[i+1]-=1,subtract.push_back((int(bigNum[i])+10-int(smalNum[i]))+'0');
-        else subtract.push_back((int(bigNum[i])-int(smalNum[i]))+'0');
-       }
-       reverse(subtract.begin(),subtract.end());
-       tmp.num=subtract;
-       tmp.del();
-       if(sign ==0) tmp.num.insert(0,"-",1);
-       return tmp;
+    string digit;
 
-
-   }
+    BigInt() {}
+    /*void setValue(string s)
+    {
+        digit = s;
+    }
+    string getValue()
+    {
+        return digit;
+    }*/
 };
-    void del(bigint b){
-        for(int i=0;i<b.num.size();++i){
-            if(b.num[i]=='0') num.erase(b.num.begin()),i--;
-            else break;
+
+
+ostream& operator<<(ostream &out, BigInt& val)
+{
+    out << val.digit;
+    return out;
+}
+
+BigInt operator+(BigInt &a,BigInt &b)
+{
+    BigInt c;
+    c.digit = "";
+    reverse(a.digit.begin(),a.digit.end());
+    reverse(b.digit.begin(),b.digit.end());
+    if (a.digit.size() > b.digit.size())
+    {
+        for (int i=b.digit.length(); i<a.digit.length(); i++)
+        {
+            b.digit+="0";
         }
     }
+    else
+    {
+        for (int i=a.digit.length(); i<b.digit.length(); i++)
+        {
+            a.digit+="0";
+        }
+    }
+    int carry = 0;
+
+    for (int i=0; i<a.digit.length(); i++)
+    {
+        int sum = 0;
+        int tmp = 0;
+        int tmp1 = 0;
+
+        tmp  = a.digit[i] - 48;
+        tmp1 = b.digit[i] - 48;
+        sum = tmp + tmp1 + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        c.digit +=(char)sum+48;
+
+    }
+    reverse(c.digit.begin(),c.digit.end());
+    return c;
+
+}
+
+BigInt operator-(BigInt &a,BigInt &b)
+{
+    BigInt c;
+    c.digit = "";
+    bool flag = false;
+    int length = b.digit.length();
+    reverse(a.digit.begin(),a.digit.end());
+    reverse(b.digit.begin(),b.digit.end());
+    if (b.digit.length()>a.digit.length())
+    {
+        flag = true;
+    }
+
+    else if((a.digit.length() == b.digit.length()) && (b.digit[length-1] > a.digit[length-1]))
+    {
+        flag = true;
+    }
+    if (a.digit.size() > b.digit.size())
+    {
+        for (int i=b.digit.length(); i<a.digit.length(); i++)
+        {
+            b.digit+="0";
+        }
+    }
+    else
+    {
+        for (int i=a.digit.length(); i<b.digit.length(); i++)
+        {
+            a.digit+="0";
+        }
+    }
+    bool carry = false;
+    if (flag == true)
+    {
+        for (int i=0; i<a.digit.length(); i++)
+        {
+            int sum = 0;
+            int tmp = 0;
+            int tmp1 = 0;
+            if (carry == true)
+            {
+                b.digit[i]-=1;
+                carry = false;
+            }
+            if ((int)b.digit[i] < (int)a.digit[i])
+            {
+                tmp  = (a.digit[i] - 48) ;
+                tmp1 = (b.digit[i] - 48) + 10 ;
+                sum = tmp1 - tmp;
+                cout << tmp1 << " " << tmp << endl;
+                c.digit +=(char)sum+48;
+                carry = true;
+
+            }
+            else
+            {
+                tmp  = (a.digit[i] - 48);
+                tmp1 = b.digit[i] - 48;
+                sum = tmp1 - tmp;
+                c.digit +=(char)sum+48;
+            }
+
+            cout << tmp1 << " " << tmp << endl;
+        }
+        c.digit+="-";
+        reverse(c.digit.begin(),c.digit.end());
+        return c;
+    }
+    else
+    {
+        for (int i=0; i<a.digit.length(); i++)
+        {
+            int sum = 0;
+            int tmp = 0;
+            int tmp1 = 0;
+            if (carry == true)
+            {
+                a.digit[i]-=1;
+                carry = false;
+            }
+            if ((int)a.digit[i] < (int)b.digit[i])
+            {
+                tmp  = (a.digit[i] - 48) + 10;
+                tmp1 = b.digit[i] - 48;
+                sum = tmp - tmp1;
+                c.digit +=(char)sum+48;
+                carry = true;
+
+            }
+            else
+            {
+                tmp  = (a.digit[i] - 48);
+                tmp1 = b.digit[i] - 48;
+                sum = tmp - tmp1;
+                c.digit +=(char)sum+48;
+            }
+
+
+        }
+
+        reverse(c.digit.begin(),c.digit.end());
+        return c;
+    }
+
+
+}
+
+
 int main()
 {
-    bigint b("12");
-    bigint b2("11");
-    bigint c;
-    c=b-b2;
-    cout<<c.print();
+    BigInt a;
+    a.digit = "111111111111111111111111111111";
+    BigInt b;
+    b.digit = "333333333333333333333333333333";
 
-
-
+    BigInt c;
+    c = a-b;
+    cout << c;
+    return 0;
 }
